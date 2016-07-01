de7bb9ce8247278cfc218de644c0813a39db673a
Aka Kherkeladze <akaki.kherkeladze@gmail.com>
2016-07-01 16:38:21 +0400

    set array of items

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class CacheCollection {
     /**
      *
      * @param {String} collectionName (required)
-     * @param {Object}options (required) {
+     * @param {Object} options (required) {
      *  expire: default expiry time span in seconds
      *  indexes: [ array of index fields ] - first index name is treated as primary index
      * }
@@ -17,7 +17,7 @@ class CacheCollection {
         //make sure indexes are supplied with options
         if (!options.indexes) options.indexes = [];
         if (typeof options.indexes === 'string') options.indexes = [options.indexes];
-        if(!options.indexes.length) throw new Error('Indexes are not supplied');
+        if (!options.indexes.length) throw new Error('Indexes are not supplied');
 
         //create cache collection if does not exists
         if (!global['CACHE_STORAGE'][collectionName]) {
@@ -42,7 +42,6 @@ class CacheCollection {
      * @param {Number} expire (optional) - expiration time in minutes, default : 1 min
      * */
     set(value, expire) {
-
         let primaryIndexValue = getKeyValue(value, this._container.primaryIndexName);
         expire = (this._options.expire || expire || 1) * 60000 + Date.now();
         this._container.primaryStorage.set(primaryIndexValue, { value, expire });
@@ -51,7 +50,6 @@ class CacheCollection {
             let secondaryIndexValue = getKeyValue(value, _);
             this._container.secondaryStorage.get(_).set(secondaryIndexValue, primaryIndexValue);
         });
-
     }
 
     /**
@@ -59,15 +57,13 @@ class CacheCollection {
      * @param {String} indexName (optional) - default is primary index
      * */
     get(key, indexName) {
-
         if(!key) throw new Error('key is required!');
 
         let record;
 
         if(!indexName || indexName == this._container.primaryIndexName) {
             record = this._container.primaryStorage.get(key);
-        }
-        else {
+        } else {
             if(!this._container.secondaryStorage.get(indexName) || !this._container.secondaryStorage.get(indexName).has(key)) return null;
 
             let primaryKey = this._container.secondaryStorage.get(indexName).get(key);
@@ -85,7 +81,6 @@ class CacheCollection {
      * @param {String} indexName (required)
      * */
     remove(key, indexName) {
-
         let primaryKey = !indexName ? key :
             this._container.secondaryStorage.get(indexName).get(key);
 
@@ -100,12 +95,17 @@ class CacheCollection {
         this._container.primaryStorage.delete(primaryKey);
         console.log('item removed...');
         return null;
-
     }
 
+    /**
+     * clear cache storage
+     * */
     clear() {
         this._container.primaryStorage.clear();
-        this._container.secondaryStorage.clear();
+
+        this._container.secondaryIndexNames.forEach(_ => {
+            this._container.secondaryStorage.get(_).clear();
+        });
     }
 
 }
